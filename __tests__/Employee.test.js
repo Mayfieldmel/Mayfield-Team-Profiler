f3a4ba0f53ab4ee8fad0d4e77ed00ef6e92eb021
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-08-06 16:45:58 -0400

    pass Employee properties test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,10 @@
+const Employee = require('../lib/Employee.js');
+
+test('creates employee object', () => {
+    const employee = new Employee('Dave', '89523', 'dave@gmail.com');
+
+    expect(employee.name).toBe("Dave");
+    expect(employee.id).toBe("89523");
+    expect(employee.email).toBe("dave@gmail.com");
+});
 
