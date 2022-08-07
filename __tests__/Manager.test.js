41bf342e06314e147d1e49f60b0bf713bc3d473d
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-08-07 11:12:31 -0400

    pass getRole test

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -4,4 +4,10 @@ test('create office number property', () => {
     const manager = new Manager('Dave', '89523', 'dave@gmail.com', '302')
 
     expect(manager.officeNumber).toBe("302");
-})
+});
+
+test("gets employee's role", () => {
+    const manager = new Manager('Dave', '89523', 'dave@gmail.com', '302');
+
+    expect(manager.getRole()).toBe("Manager");
+});
