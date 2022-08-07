c3f145c6abefc8b970b2bcaff4ff17bc2464b02a
Mayfield Mayfield <mayfieldmel@gmail.com>
2022-08-07 11:42:22 -0400

    pass getRole test

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -8,7 +8,12 @@ test('create engineer object', () => {
     expect(engineer.github).toBe("D-Dave");
 })
 
-test('get github username', () => {
+test("get engineer's github username", () => {
     const engineer = new Engineer('Dave', '89523', 'dave@gmail.com', 'D-Dave');
     expect(engineer.getGithub()).toBe('D-Dave');
+})
+
+test("get engineer's role", () => {
+    const engineer = new Engineer('Dave', '89523', 'dave@gmail.com', 'D-Dave');
+    expect(engineer.getRole()).toBe('Engineer');
 })
